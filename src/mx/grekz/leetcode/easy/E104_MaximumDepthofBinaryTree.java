5ddb39159f97351052cc1aa1965c830c4c7fb533
grekz <vidbla.grekz@gmail.com>
2018-02-07 08:56:55 +0100

    JLCE-104. Maximum Depth of Binary Tree

---
+++ src/mx/grekz/leetcode/easy/E104_MaximumDepthofBinaryTree.java
@@ -0,0 +1,13 @@
+package mx.grekz.leetcode.easy;
+
+import mx.grekz.leetcode.helpers.TreeNode;
+
+public class E104_MaximumDepthofBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return ( left > right ? left : right ) +1;
+    }
+}
