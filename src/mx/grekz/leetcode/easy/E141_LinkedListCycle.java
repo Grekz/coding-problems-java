815268b1a8c6a84d1d9fbccefb7e109657a3d936
grekz <vidbla.grekz@gmail.com>
2018-02-15 06:10:26 +0100

    JLCE-141. Linked List Cycle

---
+++ src/mx/grekz/leetcode/easy/E141_LinkedListCycle.java
@@ -0,0 +1,18 @@
+package mx.grekz.leetcode.easy;
+
+import mx.grekz.leetcode.helpers.ListNode;
+
+public class E141_LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
