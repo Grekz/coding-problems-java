afba161cb22ad013e151490bfa07814568240f27
grekz <vidbla.grekz@gmail.com>
2018-04-16 22:08:04 +0200

    JLCE-504. Base 7

---
+++ src/mx/grekz/leetcode/easy/E504_Base7.java
@@ -0,0 +1,10 @@
+package mx.grekz.leetcode.easy;
+
+/**
+ * @author grekz
+ */
+public class E504_Base7 {
+    public String convertToBase7(int num) {
+        return Integer.toString(num, 7);
+    }
+}
