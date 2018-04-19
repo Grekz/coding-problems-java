9fd5fd5eabf4cac6b9509e95c90ce55eff6218e1
grekz <vidbla.grekz@gmail.com>
2018-04-19 09:18:58 +0200

    JLCE-541. Reverse String II

--- src/mx/grekz/leetcode/easy/E541_ReverseStringII.java
+++ src/mx/grekz/leetcode/easy/E541_ReverseStringII.java
@@ -1,23 +1,21 @@
 package mx.grekz.leetcode.easy;
 
+/**
+ * @author grekz
+ */
 public class E541_ReverseStringII {
-	/**
-	 * https://leetcode.com/problems/reverse-string-ii/description/
-	 */
-	public String reverseStr(String s, int k) {
+    public String reverseStr(String s, int k) {
         char[] cArr = s.toCharArray();
         int n = cArr.length;
-        for( int i = 0; i < n; i = i + 2 * k )
-            swap(cArr, i, i + k - 1 > n - 1 ? n - 1 : i + k - 1);
+        for( int i = 0; i < n; i = i + 2 * k ){
+            int ii = i, j = i + k - 1 > n - 1 ? n - 1 : i + k - 1;
+            while(ii<j){
+                char tmp = cArr[ii];
+                cArr[ii++] = cArr[j];
+                cArr[j--] = tmp;
+            }
+        }
         
         return String.valueOf(cArr);
     }
-    private void swap(char[] arr, int i, int j){
-        while(i<j){
-            char tmp = arr[i];
-            arr[i++] = arr[j];
-            arr[j--] = tmp;
-        }
-    }
-
 }
