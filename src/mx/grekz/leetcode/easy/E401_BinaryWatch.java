e4b978ad4ee8134692b442e319c391e0bf223833
grekz <vidbla.grekz@gmail.com>
2018-04-22 23:44:01 +0100

    JLCE-401. Binary Watch

---
+++ src/mx/grekz/leetcode/easy/E401_BinaryWatch.java
@@ -0,0 +1,18 @@
+package mx.grekz.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author grekz
+ */
+public class E401_BinaryWatch {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 12; i++)
+            for (int j = 0; j < 60; j++)
+                if ( Integer.bitCount( i ) + Integer.bitCount( j ) == num )
+                    res.add(String.format("%d:%02d", i,j));
+        return res;
+    }
+}
