8e773c134fbeee85bf869f9c5273a2ebb0763587
Grekz <vidbla.grekz@gmail.com>
2019-02-03 02:28:59 +0100

    JLCE-235. Lowest Common Ancestor of a Binary Search Tree

--- src/mx/grekz/leetcode/easy/E235_LowestCommonAncestorofaBinarySearchTree.java
+++ src/mx/grekz/leetcode/easy/E235_LowestCommonAncestorofaBinarySearchTree.java
@@ -1,14 +1,13 @@
 package mx.grekz.leetcode.easy;
 
-import mx.grekz.leetcode.helpers.TreeNode;
-
 /**
  * @author grekz
  */
 public class E235_LowestCommonAncestorofaBinarySearchTree {
+
     public TreeNode lowestCommonAncestor(TreeNode r, TreeNode p, TreeNode q) {
         if( r.val > p.val && r.val > q.val ) return lowestCommonAncestor(r.left, p, q);
-        else if( r.val < p.val && r.val < q.val ) return lowestCommonAncestor(r.right, p, q);
+        if( r.val < p.val && r.val < q.val ) return lowestCommonAncestor(r.right, p, q);
         return r;
     }
 }
