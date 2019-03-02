9f6d229d6d3ed0ef85b1a9db179c85f73fe2c870
Grekz <vidbla.grekz@gmail.com>
2019-03-02 12:02:42 +0100

    JLCE-540. Single Element in a Sorted Array

---
+++ src/mx/grekz/leetcode/medium/E540_SingleElementinaSortedArray.java
@@ -0,0 +1,18 @@
+package mx.grekz.leetcode.medium;
+
+/**
+ * @author grekz
+ */
+public class E540_SingleElementinaSortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while ( i < j ) {
+            int mid = i + ( j - i ) / 2;
+            if ( nums[mid] == nums[mid ^ 1] )
+                i = mid + 1;
+            else
+                j = mid;
+        }
+        return nums[i];
+    }
+}
