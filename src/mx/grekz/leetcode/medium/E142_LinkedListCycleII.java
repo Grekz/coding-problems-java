314fe133e24a69e4935860f40f6178a1aafd5647
Grekz <vidbla.grekz@gmail.com>
2019-03-05 22:25:54 +0100

    JLCE-142. Linked List Cycle II

--- src/mx/grekz/leetcode/medium/E142_LinkedListCycleII.java
+++ src/mx/grekz/leetcode/medium/E142_LinkedListCycleII.java
@@ -5,4 +5,20 @@ package mx.grekz.leetcode.medium;
  */
 public class E142_LinkedListCycleII {
 
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while ( fast != null && fast.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if ( fast == slow ) {
+                slow = head;
+                while ( slow != fast ) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;            
+            }
+        }
+        return null;
+    }
 }
