978bf20dfa53b90a8114de237d3d1463ed601267
Grekz <vidbla.grekz@gmail.com>
2019-03-17 23:21:40 +0100

    JLCE-1013. Pairs of Songs With Total Durations Divisible by 60

---
+++ src/mx/grekz/leetcode/easy/E1013_PairsofSongsWithTotalDurationsDivisibleby60.java
@@ -0,0 +1,16 @@
+package mx.grekz.leetcode.easy;
+
+/**
+ * @author grekz
+ */
+public class E1013_PairsofSongsWithTotalDurationsDivisibleby60 {
+    public int numPairsDivisibleBy60(int[] time) {
+        int res = 0, cnt[] = new int[60];
+        for ( int x : time ) {
+            int d = (60 - x % 60) % 60;
+            res += cnt[d];
+            cnt[x % 60] += 1;
+        }
+        return res;
+    }
+}
